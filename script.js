79236a34f514248bd000b3e4cbe7899f22b90ff4
Frank Neto <frankneto481@gmail.com>
2023-06-24 13:47:36 -0400

    personal-website
    personal website created with html, css and javascript.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.sect', {rotate: {x:5,y:5,z:0}, duration:2000});
+sr.reveal('.foto', { rotate: {x:50,y:20,z:0}, duration:2200});
+sr.reveal('.sobremim', { rotate: {x:50,y:20,z:0}, duration:2000});
+sr.reveal('.sect2', { duration:2000});
+sr.reveal('.mid', { duration:2000});
+sr.reveal('.box1', {rotate: {x:30,y:40,z:0}, duration:2000});
